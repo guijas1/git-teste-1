213d7804e02cc8fa1c5137008d62c7a48ab6d1fd
guijas1 <guilherme.tavares20@outlook.com>
2024-07-25 07:58:08 -0300

    primeiro commit

---
+++ 50a280/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 250;
+        int p = 0;
+        for(int i = 50; i <= num; i++){
+
+            System.out.println(i);
+        }
+
+    }
+}
